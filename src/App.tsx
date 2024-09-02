09a07ffb3f447ce9f9a3c650eda4767e9b532b1c
roman <roman.pavlenko.job@gmail.com>
2024-09-02 12:44:06 +0300

    last steps

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,10 @@ function App() {
     swapVert: <SwapVertIcon fontSize='inherit' />
   }
 
+  const orderedArray = useMemo(() => {
+    return orderArray(usersArray, sortField, sortDirection);
+  }, [usersArray, sortField, sortDirection]);
+
   async function fetchDispatchData () {
     let result = await DataFetcher();
 
@@ -58,170 +62,169 @@ function App() {
     return copyArray;
   }
 
-  const orderedArray = useMemo(() => {
-    return orderArray(usersArray, sortField, sortDirection);
-  }, [usersArray, sortField, sortDirection]);
-
   useEffect(() => {
     fetchDispatchData();
   }, []);
 
   return (
     <div className="App">
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <TextField
-                label="Name filter"
-                value={nameFilter}
-                onChange={(event) => {
-                  dispatch(changeFilter({type: SortTypes.name, payload: event.target.value}));
-                }}
-              />
-            </TableCell>
-            <TableCell>
-              <TextField
-                label="Username filter"
-                value={usernameFilter}
-                onChange={(event) => {
-                  dispatch(changeFilter({type: SortTypes.username, payload: event.target.value}));
-                }}
-              />
-            </TableCell>
-            <TableCell>
-              <TextField
-                label="Email filter"
-                value={emailFilter}
-                onChange={(event) => {
-                  dispatch(changeFilter({type: SortTypes.email, payload: event.target.value}));
-                }}
-              />
-            </TableCell>
-            <TableCell>
-              <TextField
-                label="Phone filter"
-                value={phoneFilter}
-                onChange={(event) => {
-                  dispatch(changeFilter({type: SortTypes.phone, payload: event.target.value}));
-                }}
-              />
-            </TableCell>    
-          </TableRow>
-          <TableRow>
-            <TableCell
-              onClick={() => {
-                if (sortField !== SortTypes.name) {
-                  dispatch(changeField(SortTypes.name));
-                  dispatch(changeDirection(AscDesc.ascending));
-                } else {
-                  if (sortDirection === AscDesc.ascending) {
-                    dispatch(changeDirection(AscDesc.descending))
-                  } else {
-                    dispatch(changeDirection(AscDesc.ascending))
-                  }
-                }
-              }}
-            >
-              <div className='thead__item--wrapper'>
-                Name
-                {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.name) && iconMap.upArrow}
-                {(sortDirection === AscDesc.descending) && (sortField === SortTypes.name) && iconMap.downArrow}
-                {(sortField !== SortTypes.name) && iconMap.swapVert}
-              </div>
-            </TableCell>
-            <TableCell
-              onClick={() => {
-                if (sortField !== SortTypes.username) {
-                  dispatch(changeField(SortTypes.username));
-                  dispatch(changeDirection(AscDesc.ascending));
-                } else {
-                  if (sortDirection === AscDesc.ascending) {
-                    dispatch(changeDirection(AscDesc.descending))
-                  } else {
-                    dispatch(changeDirection(AscDesc.ascending))
-                  }
-                }
-              }}
-            >
-              <div className='thead__item--wrapper'>
-                Username
-                {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.username) && iconMap.upArrow}
-                {(sortDirection === AscDesc.descending) && (sortField === SortTypes.username) && iconMap.downArrow}
-                {(sortField !== SortTypes.username) && iconMap.swapVert}
-              </div>
-            </TableCell>
-            <TableCell
-              onClick={() => {
-                if (sortField !== SortTypes.email) {
-                  dispatch(changeField(SortTypes.email));
-                  dispatch(changeDirection(AscDesc.ascending));
-                } else {
-                  if (sortDirection === AscDesc.ascending) {
-                    dispatch(changeDirection(AscDesc.descending))
-                  } else {
-                    dispatch(changeDirection(AscDesc.ascending))
-                  }
-                }
-              }}
-            >
-              <div className='thead__item--wrapper'>
-                E-mail
-                {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.email) && iconMap.upArrow}
-                {(sortDirection === AscDesc.descending) && (sortField === SortTypes.email) && iconMap.downArrow}
-                {(sortField !== SortTypes.email) && iconMap.swapVert}
-              </div>
-            </TableCell>
-            <TableCell
-              onClick={() => {
-                if (sortField !== SortTypes.phone) {
-                  dispatch(changeField(SortTypes.phone));
-                  dispatch(changeDirection(AscDesc.ascending));
-                } else {
-                  if (sortDirection === AscDesc.ascending) {
-                    dispatch(changeDirection(AscDesc.descending))
-                  } else {
-                    dispatch(changeDirection(AscDesc.ascending))
-                  }
-                }
-              }}
-            >
-              <div className='thead__item--wrapper'>
-                Phone
-                {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.phone) && iconMap.upArrow}
-                {(sortDirection === AscDesc.descending) && (sortField === SortTypes.phone) && iconMap.downArrow}
-                {(sortField !== SortTypes.phone) && iconMap.swapVert}
-              </div>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>    
-          { orderedArray
-          .filter(el => 
-            el.name.toLowerCase().includes(nameFilter.toLowerCase().trim())
-            && el.username.toLowerCase().includes(usernameFilter.toLowerCase().trim())
-            && el.phone.toLowerCase().includes(phoneFilter.toLowerCase().trim())
-            && el.email.toLowerCase().includes(emailFilter.toLowerCase().trim())
-          )
-          .map(el => (
-            <TableRow key={el.id}>
+      <div className="container">
+        <h1 className='App__heading'>User Table</h1>
+        <Table>
+          <TableHead>
+            <TableRow>
               <TableCell>
-                {el.name}
+                <TextField
+                  label="Name filter"
+                  value={nameFilter}
+                  onChange={(event) => {
+                    dispatch(changeFilter({type: SortTypes.name, payload: event.target.value}));
+                  }}
+                />
               </TableCell>
               <TableCell>
-                {el.username}
+                <TextField
+                  label="Username filter"
+                  value={usernameFilter}
+                  onChange={(event) => {
+                    dispatch(changeFilter({type: SortTypes.username, payload: event.target.value}));
+                  }}
+                />
               </TableCell>
               <TableCell>
-                {el.email}
+                <TextField
+                  label="Email filter"
+                  value={emailFilter}
+                  onChange={(event) => {
+                    dispatch(changeFilter({type: SortTypes.email, payload: event.target.value}));
+                  }}
+                />
               </TableCell>
               <TableCell>
-                {el.phone}
+                <TextField
+                  label="Phone filter"
+                  value={phoneFilter}
+                  onChange={(event) => {
+                    dispatch(changeFilter({type: SortTypes.phone, payload: event.target.value}));
+                  }}
+                />
+              </TableCell>    
+            </TableRow>
+            <TableRow>
+              <TableCell
+                onClick={() => {
+                  if (sortField !== SortTypes.name) {
+                    dispatch(changeField(SortTypes.name));
+                    dispatch(changeDirection(AscDesc.ascending));
+                  } else {
+                    if (sortDirection === AscDesc.ascending) {
+                      dispatch(changeDirection(AscDesc.descending))
+                    } else {
+                      dispatch(changeDirection(AscDesc.ascending))
+                    }
+                  }
+                }}
+              >
+                <div className='thead__item--wrapper'>
+                  Name
+                  {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.name) && iconMap.upArrow}
+                  {(sortDirection === AscDesc.descending) && (sortField === SortTypes.name) && iconMap.downArrow}
+                  {(sortField !== SortTypes.name) && iconMap.swapVert}
+                </div>
+              </TableCell>
+              <TableCell
+                onClick={() => {
+                  if (sortField !== SortTypes.username) {
+                    dispatch(changeField(SortTypes.username));
+                    dispatch(changeDirection(AscDesc.ascending));
+                  } else {
+                    if (sortDirection === AscDesc.ascending) {
+                      dispatch(changeDirection(AscDesc.descending))
+                    } else {
+                      dispatch(changeDirection(AscDesc.ascending))
+                    }
+                  }
+                }}
+              >
+                <div className='thead__item--wrapper'>
+                  Username
+                  {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.username) && iconMap.upArrow}
+                  {(sortDirection === AscDesc.descending) && (sortField === SortTypes.username) && iconMap.downArrow}
+                  {(sortField !== SortTypes.username) && iconMap.swapVert}
+                </div>
+              </TableCell>
+              <TableCell
+                onClick={() => {
+                  if (sortField !== SortTypes.email) {
+                    dispatch(changeField(SortTypes.email));
+                    dispatch(changeDirection(AscDesc.ascending));
+                  } else {
+                    if (sortDirection === AscDesc.ascending) {
+                      dispatch(changeDirection(AscDesc.descending))
+                    } else {
+                      dispatch(changeDirection(AscDesc.ascending))
+                    }
+                  }
+                }}
+              >
+                <div className='thead__item--wrapper'>
+                  E-mail
+                  {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.email) && iconMap.upArrow}
+                  {(sortDirection === AscDesc.descending) && (sortField === SortTypes.email) && iconMap.downArrow}
+                  {(sortField !== SortTypes.email) && iconMap.swapVert}
+                </div>
+              </TableCell>
+              <TableCell
+                onClick={() => {
+                  if (sortField !== SortTypes.phone) {
+                    dispatch(changeField(SortTypes.phone));
+                    dispatch(changeDirection(AscDesc.ascending));
+                  } else {
+                    if (sortDirection === AscDesc.ascending) {
+                      dispatch(changeDirection(AscDesc.descending))
+                    } else {
+                      dispatch(changeDirection(AscDesc.ascending))
+                    }
+                  }
+                }}
+              >
+                <div className='thead__item--wrapper'>
+                  Phone
+                  {(sortDirection === AscDesc.ascending) && (sortField === SortTypes.phone) && iconMap.upArrow}
+                  {(sortDirection === AscDesc.descending) && (sortField === SortTypes.phone) && iconMap.downArrow}
+                  {(sortField !== SortTypes.phone) && iconMap.swapVert}
+                </div>
               </TableCell>
             </TableRow>
-          ))         
-          }
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>    
+            { orderedArray
+            .filter(el => 
+              el.name.toLowerCase().includes(nameFilter.toLowerCase().trim())
+              && el.username.toLowerCase().includes(usernameFilter.toLowerCase().trim())
+              && el.phone.toLowerCase().includes(phoneFilter.toLowerCase().trim())
+              && el.email.toLowerCase().includes(emailFilter.toLowerCase().trim())
+            )
+            .map(el => (
+              <TableRow key={el.id}>
+                <TableCell>
+                  {el.name}
+                </TableCell>
+                <TableCell>
+                  {el.username}
+                </TableCell>
+                <TableCell>
+                  {el.email}
+                </TableCell>
+                <TableCell>
+                  {el.phone}
+                </TableCell>
+              </TableRow>
+            ))         
+            }
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
